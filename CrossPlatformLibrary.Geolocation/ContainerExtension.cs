d2a1a209322ef2c2c2484ca05a713fd78f1f74f5
thomasgalliker <thomas@bluewin.ch>
2015-08-09 11:56:26 +0000

    Added ContainerExtension

---
+++ CrossPlatformLibrary.Geolocation/ContainerExtension.cs
@@ -0,0 +1,13 @@
+﻿using CrossPlatformLibrary.Bootstrapping;
+using CrossPlatformLibrary.IoC;
+
+namespace CrossPlatformLibrary.Geolocation
+{
+    public class ContainerExtension : IContainerExtension
+    {
+        public void Initialize(ISimpleIoc container)
+        {
+            container.RegisterPlatformSpecific<ILocationService>();
+        }
+    }
+}
