88107d377584f1563a69ec90f2cf3f4230765971
thomasgalliker <thomas@bluewin.ch>
2016-01-12 17:34:00 +0100

    Add sample apps (Xamarin Forms)

---
+++ Samples/GeolocationSample/GeolocationSample/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using CrossPlatformLibrary.Bootstrapping;
+using CrossPlatformLibrary.ExceptionHandling;
+
+using Xamarin.Forms;
+
+namespace GeolocationSample
+{
+    public class App : Application
+    {
+        private readonly Bootstrapper bootstrapper;
+
+        public App()
+        {
+            this.bootstrapper = new Bootstrapper();
+
+            this.MainPage = new DemoPage();
+        }
+
+        protected override void OnStart()
+        {
+            this.bootstrapper.Startup();
+        }
+
+        protected override void OnSleep()
+        {
+            this.bootstrapper.Sleep();
+        }
+
+        protected override void OnResume()
+        {
+            this.bootstrapper.Resume();
+        }
+    }
+}
---
+++ Samples/GeolocationSample/GeolocationSample/DemoPage.cs
@@ -0,0 +1,43 @@
+﻿using CrossPlatformLibrary.Geolocation;
+using CrossPlatformLibrary.IoC;
+
+using Xamarin.Forms;
+
+namespace GeolocationSample
+{
+    class DemoPage : ContentPage
+    {
+        public DemoPage()
+        {
+            var locationLabel = new Label { Text = "Press button to get GPS location" };
+
+            var getPositionButton = new Button
+            {
+                Text = "Get GPS location"
+            };
+            getPositionButton.Clicked += async (sender, args) =>
+            {
+                ILocationService locationService = SimpleIoc.Default.GetInstance<ILocationService>();
+                if (!locationService.IsGeolocationEnabled)
+                {
+                    var position = await locationService.GetPositionAsync();
+                    locationLabel.Text = position.ToString();
+                }
+                else
+                {
+                    ILocationServiceSettings locationServiceSettings = SimpleIoc.Default.GetInstance<ILocationServiceSettings>();
+                    locationServiceSettings.Show();
+                }
+            
+            };
+            
+            var stackPanel = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { getPositionButton, locationLabel }
+            };
+
+            this.Content = stackPanel;
+        }
+    }
+}
