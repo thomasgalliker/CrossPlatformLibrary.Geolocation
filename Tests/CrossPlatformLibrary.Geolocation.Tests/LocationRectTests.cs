cc3342795e4366a3eb6a8e200bf138e0c65ed9e4
thomasgalliker <thomas@bluewin.ch>
2016-04-14 16:09:55 +0200

    Add new data type LocationRect to represent rectangular location areas

---
+++ Tests/CrossPlatformLibrary.Geolocation.Tests/LocationRectTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace CrossPlatformLibrary.Geolocation.Tests
+{
+    public class LocationRectTests
+    {
+        [Fact]
+        public void ShouldContainsZeroLatitudeLongitudeInZeroLocationRect()
+        {
+            // Arrange
+            var locationRect = new LocationRect(0,0,0,0);
+
+            // Act
+            var isContained = locationRect.Contains(new Position(0, 0));
+
+            // Assert
+            isContained.Should().BeTrue();
+        }
+    }
+}
